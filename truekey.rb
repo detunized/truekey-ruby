5f1e62ac90474287e543a8104567b8f77598f336
Dmitry Yakimenko <detunized@gmail.com>
2017-03-13 13:53:31 +0100

    Http class

--- truekey.rb
+++ truekey.rb
@@ -1,6 +1,23 @@
 require "yaml"
 require "httparty"
 
+class Http
+    include HTTParty
+
+    def get url, headers = {}
+        self.class.get url, {
+            headers: headers
+        }
+    end
+
+    def post url, args, headers = {}
+        self.class.post url, {
+            body: args,
+            headers: headers
+        }
+    end
+end
+
 #
 # main
 #
