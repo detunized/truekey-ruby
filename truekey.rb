5c52423ac96209ac9af69df503965fd51a957b59
Dmitry Yakimenko <detunized@gmail.com>
2017-03-14 11:38:20 +0100

    Request client token

--- truekey.rb
+++ truekey.rb
@@ -1,3 +1,4 @@
+require "json"
 require "yaml"
 require "httparty"
 
@@ -16,10 +17,71 @@ class Http
             headers: headers
         }
     end
+
+    def post_json url, args, headers = {}
+        post url, args.to_json, headers.merge({"Content-Type" => "application/json"})
+    end
+end
+
+# This is the first step in authentication process for a new device.
+# This requests the client token with is used in OCRA (RFC 6287) exchange
+# later on. There's also a server assigned id for the new device.
+#
+# `device_name` is the name of the device registered with the True Key service.
+# For example 'Chrome' or 'Nexus 5'.
+
+def register_new_device device_name, http
+    response = http.post_json "https://truekeyapi.intelsecurity.com/sp/pabe/v2/so", {
+              clientUDID: "truekey-ruby", # The official client generates random client
+                                          # UDID every time. It doesn't seem to be needed,
+                                          # so it's just hardcoded here.
+              deviceName: device_name,
+        devicePlatformID: 7,              # MacOS (see DevicePlatformType)
+              deviceType: 5,              # Mac (see DeviceType)
+                  oSName: "Unknown",
+           oathTokenType: 1
+    }
+
+    # Response example:
+    #
+    # parsed = {
+    #      "responseResult" => {
+    #                "isSuccess" => true,
+    #                "errorCode" => "",
+    #         "errorDescription" => nil,
+    #            "transactionId" => "e36c90af-9f73-4354-af78-902dfa80bd87"
+    #     },
+    #         "clientToken" => "AQCmAwEAAh4AAAAAWMajHQAAGU9DUkEtMTpIT1RQLVNIQ" +
+    #                          "TI1Ni0wOlFBMDgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" +
+    #                          "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" +
+    #                          "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIOiR" +
+    #                          "fItpCTOkvq0ZfV2+GgvP83aF9SrTBfOuabZfcQr9AAAAA" +
+    #                          "AgAIBwWTZpUTIn493Us/JwczrK6O0+LH8FRidFaZkJ2Al" +
+    #                          "Tu",
+    #          "tkDeviceId" => "d871347bd0a3e7af61f60f511bc7de5e944c5c7787056" +
+    #                          "49d4aa8dc77bcd21489412894",
+    #       "tkDeviceNonce" => "1489412894",
+    #     "tkReEnrollToken" => "bd23b315af167d81ea216a1c26a1a836657583e84fece" +
+    #                          "a2fb3281920a33aec84"
+    # }
+
+    parsed = response.parsed_response
+    raise "Request failed" if !parsed["responseResult"]["isSuccess"]
+
+    result = {
+        token: parsed["clientToken"],
+        id: parsed["tkDeviceId"]
+    }
+    raise "Invalid response" if result.values.include? nil
+
+    result
 end
 
 #
 # main
 #
 
-config = ap YAML::load_file "config.yaml"
+config = YAML::load_file "config.yaml"
+
+http = Http.new
+device_info = register_new_device "truekey-ruby", http
