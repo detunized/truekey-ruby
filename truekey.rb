da2eef268f2422ee4bc21c2feee11074c658e9ec
Dmitry Yakimenko <detunized@gmail.com>
2017-03-15 12:56:09 +0100

    Copyright header

--- truekey.rb
+++ truekey.rb
@@ -1,5 +1,8 @@
 #!/usr/bin/env ruby
 
+# Copyright (C) 2017 Dmitry Yakimenko (detunized@gmail.com).
+# Licensed under the terms of the MIT license. See LICENCE for details.
+
 require "json"
 require "yaml"
 require "httparty"
